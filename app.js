c0f36bd782bcd31f4489bf933361e85579561b4b
Lloyd Ntim <kwame.23@gmail.com>
2015-05-30 12:12:04 +0200

    Re-added files including plubic folder

--- app.js
+++ app.js
@@ -1,51 +1,13 @@
 var express = require('express');
-var http = require('http');
-var path = require('path');
-var static = require('serve-static');
-var bodyParser = require( 'body-parser' );
-
-var nodemailer = require('nodemailer');
-
-// reusable transporter object using SMTP transport
-var transporter = nodemailer.createTransport('SMTP', {
-    service: 'Gmail',
-    auth: {
-        XOAuth2: {
-            user: 'kwame.23@gmail.com',
-            clientId: '830781104220.apps.googleusercontent.com',
-            clientSecret: 'qf6Yuk8-qkSAUitbEmd3eOTO',
-            refreshToken: '1/JG9oE1i7XyLwyccKtbqfda-K_OczHC0MXdbDzgRU3Zs'
-        }
-    }
-});
-
-// create application/json parser
-//var jsonParser = bodyParser.json();
-
-// create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({extended: false});
-
 var app = express();
-var routes = require('./routes');
 
-// set our port
-var port = process.env.PORT || 8002;
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
-app.use(static(path.join(__dirname, 'dist')));
-
-// Routes
-app.get('/', routes.index);
-
-app.get('/admin', function(req, res) {
-    console.log('ad min');
-    res.sendStatus(200);
+app.get('/', function(request, response) {
+    response.send('Hello World!')
 });
 
-app.post('/sendquestion', urlencodedParser, routes.sendquestion(transporter));
-
-app.on('listening',function(){
-    console.log('ok, server is running');
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'));
 });
-
-app.listen(port);
-console.log('Magic happens on port ' + port);
