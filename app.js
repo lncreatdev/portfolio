01711bd14e3d03138a98a8fdc69ffe20042db7c5
Lloyd Ntim <kwame.23@gmail.com>
2015-05-31 10:23:08 +0200

    server side update

--- app.js
+++ app.js
@@ -4,17 +4,7 @@ var path = require('path');
 var static = require('serve-static');
 var bodyParser = require( 'body-parser' );
 
-var nodemailer = require('nodemailer');
-var mg = require('nodemailer-mailgun-transport');
-
-var auth = {
-    auth: {
-        api_key: 'key-8936eca331f064d3f56edb74e900c0ea',
-        domain: 'sandbox06eedd3a1f594d51890db3d62aa368cc.mailgun.org'
-    }
-};
-
-var transporter = nodemailer.createTransport(mg(auth));
+var sendgrid  = require('sendgrid')(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
 
 // create application/json parser
 //var jsonParser = bodyParser.json();
